acc0d27822fcf6252328da7794a88d952d0d2b03
Mikolaj Radkowski <mikolaj.radkowski@protonmail.com>
2024-01-22 01:50:47 +0100

    Sending messages to users

--- Assets/Scripts/AccountManager.cs
+++ Assets/Scripts/AccountManager.cs
@@ -6,8 +6,12 @@ public class AccountManager : MonoBehaviour
 {
     public Account[] accounts;
     public Account currentAccount;
+    
     public GameObject mainContent;
-
+    public GameObject teacherSentMessages;
+    public GameObject teacherReceivedMessages;
+    public GameObject teacherMessagesOptions;
+    public GameObject studentMessagesOptions;
 
 	private void Start()
 	{
@@ -32,6 +36,20 @@ public class AccountManager : MonoBehaviour
 	public void SwitchAccount(Account acc)
 	{
 		currentAccount = acc;
+		if (currentAccount.type == Enums.AccountType.Nauczyciel)
+		{
+			teacherReceivedMessages.SetActive(true);	
+			teacherSentMessages.SetActive(false);
+			teacherMessagesOptions.SetActive(true);
+			studentMessagesOptions.SetActive(false);
+		}
+		else
+		{
+			teacherReceivedMessages.SetActive(false);	
+			teacherSentMessages.SetActive(true);
+			studentMessagesOptions.SetActive(true);
+			teacherMessagesOptions.SetActive(false);
+		}
 	}
 
 	public void Logout()
--- Assets/Scripts/SendMessage.cs
+++ Assets/Scripts/SendMessage.cs
@@ -7,34 +7,46 @@ using UnityEngine.UI;
 
 public class SendMessage : MonoBehaviour
 {
-    public GameObject receivedMessagesPage;
+    public GameObject receivedMessagesPageStudent;
+    public GameObject receivedMessagesPageTeacher;
     public GameObject messagesRoot;
     public GameObject message;
 
+    public AccountManager accountManager;
     public SelectiveActivation selectiveActivation;
-    
+
     public TMP_InputField receiver;
     public TMP_InputField title;
     public TMP_InputField content;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
+
     public void Send()
     {
-        GameObject newMessage = Instantiate(message, messagesRoot.transform);
-        newMessage.GetComponent<MessageSetup>().SetMessage(receiver.text, title.text, content.text);
+        // TODO: Allow sending messages only to existing users (maybe use select box)
         
-        selectiveActivation.SelectActive(receivedMessagesPage);
+        if (accountManager.currentAccount.type == Enums.AccountType.Uczen)
+        {
+            GameObject newMessage = Instantiate(message, messagesRoot.transform);
+            newMessage.GetComponent<MessageSetup>().SetMessage(receiver.text, title.text, content.text);
+            selectiveActivation.SelectActive(receivedMessagesPageStudent);
+        }
+        else
+        {
+            // God save us. It's so hacky
+            GameObject newMessage = Instantiate(message, receivedMessagesPageStudent.transform);
+            newMessage.GetComponent<MessageSetup>().SetMessage(receiver.text, title.text, content.text);
+            selectiveActivation.SelectActive(receivedMessagesPageTeacher);
+        }
+
         receiver.text = "";
         title.text = "";
         content.text = "";
